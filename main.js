93e3be347c132c4616dda50849b1a85a8fdeae88
Keg <38766915+kegdotcom@users.noreply.github.com>
2023-01-18 09:30:50 -0600

    added persistant goals

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+if (localStorage.getItem("calorie-goal") != null) {
+    document.getElementById('calorie-goal').innerHTML = localStorage.getItem("calorie-goal");
+} else {
+    document.getElementById('protein-goal').innerHTML = 'None';
+}
+
+if (localStorage.getItem("protein-goal") != null) {
+    document.getElementById('protein-goal').innerHTML = localStorage.getItem("protein-goal");
+} else {
+    document.getElementById('protein-goal').innerHTML = 'None';
+}
+
+if (localStorage.getItem("fat-goal") != null) {
+    document.getElementById('fat-goal').innerHTML = localStorage.getItem("fat-goal");
+} else {
+    document.getElementById('fat-goal').innerHTML = 'None';
+}
+
+if (localStorage.getItem("carb-goal") != null) {
+    document.getElementById('carb-goal').innerHTML = localStorage.getItem("carb-goal");
+} else {
+    document.getElementById('carb-goal').innerHTML = 'None';
+}
+
 function updateCalories() {
     localStorage.setItem("calorie-goal", document.getElementById('new-calorie-goal').value);
     document.getElementById('calorie-goal').innerHTML = localStorage.getItem("calorie-goal");
